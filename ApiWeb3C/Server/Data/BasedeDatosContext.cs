109608467527c72efdaf67142ac5125e7ba10f35
Pedro Damian <damian@ucol.mx>
2023-11-16 14:45:39 -0600

    Agregar archivos de proyecto.

---
+++ ApiWeb3C/Server/Data/BasedeDatosContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiWeb3C.Shared.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWeb3C.Server.Data
+{
+    public class BasedeDatosContext : DbContext
+    {
+        public BasedeDatosContext(DbContextOptions<BasedeDatosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ ApiWeb3C/Shared/Modelos/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiWeb3C.Shared.Modelos
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Debe escribir el nombre")]
+        [StringLength(100)]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage ="Debe escribir el teléfono")]
+        [StringLength(20)]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage ="Debe escribir el correo")]
+        [StringLength(50)]
+        public string? Correo { get; set; }
+    }
+}
